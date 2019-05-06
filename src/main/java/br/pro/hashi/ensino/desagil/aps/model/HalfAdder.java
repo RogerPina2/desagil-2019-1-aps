016c44fd9593f15d09ec9df120e5cf357c526dd9
RogerPina <rogerrfp@al.insper.edu.br>
2019-05-06 19:08:32 -0300

    Questão 2

--- src/main/java/br/pro/hashi/ensino/desagil/aps/model/HalfAdder.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/model/HalfAdder.java
@@ -1,4 +1,57 @@
 package br.pro.hashi.ensino.desagil.aps.model;
 
 public class HalfAdder extends Gate {
+    private final NandGate nandLeft;
+    private final NandGate nandTop;
+    private final NandGate nandRight;
+    private final NandGate nandBottom;
+    private final NandGate nandBottomBottom;
+
+    public HalfAdder() {
+        super("Half-Adder", 2, 2);
+
+        nandLeft = new NandGate();
+
+        nandTop = new NandGate();
+        nandTop.connect(1, nandLeft);
+
+        nandBottom = new NandGate();
+        nandBottom.connect(0, nandLeft);
+
+        nandRight = new NandGate();
+        nandRight.connect(0, nandTop);
+        nandRight.connect(1, nandBottom);
+
+        nandBottomBottom = new NandGate();
+        nandBottomBottom.connect(0, nandLeft);
+        nandBottomBottom.connect(1, nandLeft);
+    }
+
+    @Override
+    public boolean read(int outputPin) {
+        switch (outputPin) {
+            case 0:
+                return nandRight.read();
+            case 1:
+                return nandBottomBottom.read();
+        }
+        return false;
+    }
+
+
+    @Override
+    public void connect(int inputPin, SignalEmitter emitter) {
+        switch (inputPin) {
+            case 0:
+                nandTop.connect(0, emitter);
+                nandLeft.connect(0, emitter);
+                break;
+            case 1:
+                nandLeft.connect(1, emitter);
+                nandBottom.connect(1, emitter);
+                break;
+            default:
+                throw new IndexOutOfBoundsException(inputPin);
+        }
+    }
 }
--- src/main/java/br/pro/hashi/ensino/desagil/aps/view/GateView.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/view/GateView.java
@@ -20,7 +20,7 @@ public class GateView extends FixedPanel implements ItemListener {
     private final Switch[] switches;
     private final Gate gate;
     private final JCheckBox[] inputBoxes;
-    private final JCheckBox outputBox;
+    private final JCheckBox[] outputBoxes;
     private final Image image;
 
     public GateView(Gate gate) {
@@ -40,7 +40,13 @@ public class GateView extends FixedPanel implements ItemListener {
             gate.connect(i, switches[i]);
         }
 
-        outputBox = new JCheckBox();
+        int outputSize = gate.getOutputSize();
+
+        outputBoxes = new JCheckBox[outputSize];
+
+        for (int i = 0; i < outputSize; i++) {
+            outputBoxes[i] = new JCheckBox();
+        }
 
         int x, y, step;
 
@@ -52,7 +58,13 @@ public class GateView extends FixedPanel implements ItemListener {
             add(inputBox, x, y, SWITCH_SIZE, SWITCH_SIZE);
         }
 
-        add(outputBox, BORDER + SWITCH_SIZE + GATE_WIDTH, (GATE_HEIGHT - SWITCH_SIZE) / 2, SWITCH_SIZE, SWITCH_SIZE);
+        x = BORDER;
+        y = -(SWITCH_SIZE / 2);
+        step = (GATE_HEIGHT / (outputSize + 1));
+        for (JCheckBox outputBox : outputBoxes) {
+            y += step;
+            add(outputBox, BORDER + SWITCH_SIZE + GATE_WIDTH, y, SWITCH_SIZE, SWITCH_SIZE);
+        }
 
         String name = gate.toString() + ".png";
         URL url = getClass().getClassLoader().getResource(name);
@@ -62,7 +74,9 @@ public class GateView extends FixedPanel implements ItemListener {
             inputBox.addItemListener(this);
         }
 
-        outputBox.setEnabled(false);
+        for (JCheckBox outputBox : outputBoxes) {
+            outputBox.setEnabled(false);
+        }
 
         update();
     }
@@ -76,9 +90,12 @@ public class GateView extends FixedPanel implements ItemListener {
             }
         }
 
-        boolean result = gate.read();
+        for (int i = 0; i < gate.getOutputSize(); i++) {
 
-        outputBox.setSelected(result);
+            boolean result = gate.read(i);
+
+            outputBoxes[i].setSelected(result);
+        }
     }
 
     @Override
